fb2c6e67c8a3a7b9faa0dfb658d779d713ac6516
aliBenhenia <alibenhenia1@gmail.com>
2025-08-21 01:40:05 +0200

    feat:

---
+++ pusher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if commit message is provided
+if [ -z "$1" ]; then
+  echo "❌ Please provide a commit message."
+  exit 1
+fi
+
+# Git workflow
+git add .
+git commit -m "$1"
+git push
